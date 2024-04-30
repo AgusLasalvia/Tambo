094f197b18e93f2f94872c9fc724d120bc2de839
FacuColombi <127901758+FacuColombi@users.noreply.github.com>
2024-04-30 19:43:22 -0300

    asignar-tareas

--- Console/obj/Debug/net8.0/Console.AssemblyInfo.cs
+++ Console/obj/Debug/net8.0/Console.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Console")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+004ba388d865472e2329700351b1153ae5bef5ac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27bff22c1f4c6de3e337337e10e9f256c50924a6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Console")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Console")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
