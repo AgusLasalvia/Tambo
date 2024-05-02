89d86fc3dbec00cc656bd1a78e20e62711b6148d
AgusLasalvia <aguslblumenfeld@gmail.com>
2024-05-01 21:41:51 -0300

    progress

--- Dominio/Peon.cs
+++ Dominio/Peon.cs
@@ -2,23 +2,31 @@
 namespace Dominio
 {
     class Peon : Empleado, IValidable
-    {   
+    {
         // Atributos 
         private bool _reside;
-        private List<Tarea> _tareas;
+        private List<Tarea>? _tareas;
 
-        
+
         // Constructor
         public Peon(bool reside, string email, string password, string nombre, DateTime fechaIngreso) : base(email, password, nombre, fechaIngreso)
-        {   
+        {
             _reside = reside;
             _tareas = new List<Tarea>(); // Genera una lista pero vacia
         }
 
-        public void AgregarTarea(Tarea tarea){
+        public void AgregarTarea(Tarea tarea)
+        {
             _tareas.Add(tarea);
         }
 
+        public void ListarTareas()
+        {
+            foreach (Tarea tarea in _tareas)
+            {
+                Console.WriteLine(tarea);
+            }
+        }
 
         //Funcion que sobre escribe la funcion ToString()
         public override string ToString()
@@ -28,10 +36,11 @@ namespace Dominio
 
 
         //Funcion que sobre escribe la funcion Validar() de su clase padre
-        public override void Validar(){
-			base.Validar();
-			if (_tareas == null) throw new Exception("El peon debe tener una lista de tareas");
-		}
-		
+        public override void Validar()
+        {
+            base.Validar();
+            if (_tareas == null) throw new Exception("El peon debe tener una lista de tareas");
+        }
+
     }
 }
